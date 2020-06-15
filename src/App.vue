a494ed2769dc3af0544c457b6b57354fd2cfc493
Masaya Kazama <miyan@t-p.jp>
2020-06-16 02:24:38 +0900

    2点クリックで入れ替わるデモ作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    <h3>2点クリックで入れ替わり</h3>
+
+    <ul class="items">
+      <li
+        v-for="(item, i) in list"
+        :key="i"
+        class="item"
+        @click="() => select(i)"
+      >
+        {{ item }} ({{ i }})
+      </li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from 'vue'
+import { mv } from '@/utils/list'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default Vue.extend({
+  data() {
+    return {
+      list: 'ABCDE'.split(''),
+      selectedIndex: -1,
     }
-  }
+  },
+  methods: {
+    select(index: number) {
+      // 移動元が選択されてない->移動元選択
+      if (this.selectedIndex < 0) {
+        this.selectedIndex = index
+        return
+      }
+      // 移動元を選択済み->移動実行
+      this.list = mv(this.list, this.selectedIndex, index)
+      this.selectedIndex = -1
+    },
+  },
+})
+</script>
+<style scoped>
+.items {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.item {
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 4px;
 }
 </style>
